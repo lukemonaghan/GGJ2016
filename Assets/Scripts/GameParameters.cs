08eb83fc0d0abd3265fd919fef9146f9d679f023
Luke Monaghan <luke.p.monaghan@gmail.com>
2016-01-29 19:37:10 +1100

    Initial Commit

---
+++ Assets/Scripts/GameParameters.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// A place to store miscellaneous global parameters.
+public class GameParameters : ScriptableObject 
+{
+    // Convenience method for accessing GameParameters asset
+    public static GameParameters Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = (GameParameters)Resources.Load("GameParameters", typeof(GameParameters));
+            return _instance;
+        }
+    }
+    static GameParameters _instance;
+
+
+
+}
